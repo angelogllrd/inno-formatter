30c95bcc472a9b4ba999eaf823126f73c59a915b
angelogllrd <angelogallardi@hotmail.com>
2026-08-31 16:22:05 -0300

    agregada detección de "Flags: unchecked" y "skipifsilent " para instalación silenciosa

--- inno-formatter.py
+++ inno-formatter.py
@@ -1,6 +1,9 @@
 #! python3
-# Formatea el script de creación del instalador de Inno Setup para solucionar
-# el problema de las carpetas, y lo copia al portapapeles.
+# Formatea el script de creación del instalador de Inno Setup para:
+#   * solucionar el problema de las carpetas
+#   * tildar el checkbox de ícono en escritorio
+#   * iniciar automáticamente si la instalación es /SILENT
+# y lo copia al portapapeles.
 
 # By Angelo Gallardi
 
@@ -12,20 +15,30 @@ script = pyperclip.paste()
 # Separo el texto en lineas
 lineas = script.splitlines()
 
-# Formateo las lineas de carpetas
 salida = []
 for linea in lineas:
-	if 'recursesubdirs createallsubdirs' in linea: # Las lineas de carpetas contienen las 2 palabras
+	# 1. Verifico "Flags: unchecked"
+	if 'Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}";' in linea:
+		salida.append(linea.replace(' Flags: unchecked', ''))
+
+	# 2. Verifico linea de carpeta
+	elif 'recursesubdirs createallsubdirs' in linea:
 		# Busco el nombre de la carpeta
 		mo = re.search(r'\\([^\\]+)\\\*";', linea) # Regex para detectar el patrón "\nombre_carpeta\*"
 		carpeta = mo.group(1)
 
 		# Formateo la linea reemplazando "{app}" por "{app}/nombre_carpeta" y la agrego a las demás
 		salida.append(linea.replace('{app}', '{app}/' + carpeta))
+
+	# 3. Verifico linea de ejecución al terminar instalación
+	elif 'Flags: nowait postinstall skipifsilent' in linea:
+		salida.append(linea.replace(' skipifsilent', ''))
+
+	# 4. No debo modificar nada, copio tal cual
 	else:
 		salida.append(linea)
 
 # Formo nuevamente el script y lo copio al portapapeles
 script = '\n'.join(salida)
-print('Script formateado copiado al portapapeles.')
 pyperclip.copy(script)
+print('Script formateado copiado al portapapeles.')
